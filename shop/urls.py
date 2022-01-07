3773e581cc88fa4893ca66fba0c86f892b4ba25e
Eunji Nam <godangel31@gmail.com>
2022-01-07 09:55:16 +0900

    shop 앱 기본 구현

--- shop/urls.py
+++ shop/urls.py
@@ -1,3 +1,13 @@
-app_name = 'shop'
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from shop.views import ReviewViewSet
 
-urlpatterns = []
+
+app_name = "shop"
+
+router = DefaultRouter()
+router.register("reviews", ReviewViewSet)
+
+urlpatterns = [
+    path("api/", include(router.urls)),
+]
