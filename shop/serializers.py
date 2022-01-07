5860c71790fc90c1a102bcde0931b6acfce53425
Eunji Nam <godangel31@gmail.com>
2022-01-07 10:39:40 +0900

    shop_list와 shop_new 뷰 구현

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from shop.models import Review
 
+# DRF(djangorestframework) 를 이용할 때는 form 이 아니라 serializer 사용
+
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
