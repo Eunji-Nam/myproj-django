d6289ed396e522ace4ded0d349dbffa50c359f1a
Eunji Nam <godangel31@gmail.com>
2022-01-18 10:17:02 +0900

    access/refresh 속성 외에 추가 속성

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,4 @@
 from typing import Dict
-
 from rest_framework_simplejwt.serializers import (
     TokenObtainPairSerializer as OrigTokenObtainPairSerializer,
     TokenRefreshSerializer as OrigTokenRefreshSerializer,
@@ -7,13 +6,25 @@ from rest_framework_simplejwt.serializers import (
 
 
 class TokenObtainPairSerializer(OrigTokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user) -> Dict:
-        token = super().get_token(user)
-        token['username'] = user.username
-        token['first_name'] = user.first_name
-        token['last_name'] = user.last_name
-        return token
+
+    def validate(self, attrs):
+        # access/refresh 속성 외에 추가 속성
+        data = super().validate(attrs)
+        data['username'] = self.user.username
+        data['first_name'] = self.user.first_name
+        data['last_name'] = self.user.last_name
+        # TODO: 프로필 이미지 URL
+        return data
+
+
+# JWT Payload 커스텀
+    # @classmethod
+    # def get_token(cls, user) -> Dict:
+    #     token = super().get_token(user)
+    #     token['username'] = user.username
+    #     token['first_name'] = user.first_name
+    #     token['last_name'] = user.last_name
+    #     return token
 
 
 class TokenRefreshSerializer(OrigTokenRefreshSerializer):
